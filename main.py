b5eb3e76a1382cc7a179a21959644f357bd617b8
25thOliver <os679736@gmail.com>
2026-02-05 06:27:52 +0300

    Add the main, entry point

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import time
+
+def main():
+    print("ETL Application pipeline initialized.")
+    # Placeholder for logic
+    while True:
+        time.sleep(10)
+
+if __name__ == "__main__":
+    main()
