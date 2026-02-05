a5d8bb7c1e7c33b1fcc06d40ededfd8420ae7b62
25thOliver <os679736@gmail.com>
2026-02-05 06:49:17 +0300

    App configuration

--- etl_pipeline/config.py
+++ etl_pipeline/config.py
@@ -11,9 +11,19 @@ class Config:
     POSTGRES_DB = os.getenv("POSTGRES_DB", "supermarket")
     POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
     POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
-    
+
     MONGO_USER = os.getenv("MONGO_USER", "admin")
     MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "password")
     MONGO_DB = os.getenv("MONGO_DB", "supermarket")
     MONGO_HOST = os.getenv("MONGO_HOST", "localhost")
     MONGO_PORT = os.getenv("MONGO_PORT", "27017")
+
+    # App Config
+    DATA_SOURCE_TYPE = os.getenv("DATA_SOURCE_TYPE", "sheets")
+
+    # Google Sheets Config
+    # The sheet ID extracted from the URL
+    https://docs.google.com/spreadsheets/d/1CHSfRQTla3Kkang7E_PptCKc6WYMIlzwDoe_hgMMajE/edit?usp=sharing
+    GOOGLE_SHEET_ID = os.getenv("GOOGLE_SHEET_ID", "1CHSfRQTla3Kkang7E_PptCKc6WYMIlzwDoe_hgMMajE")
+    GOOGLE_SHEETS_CREDENTIALS = os.getenv("GOOGLE_SHEETS_CREDENTIALS", "credentials.json")
+    
